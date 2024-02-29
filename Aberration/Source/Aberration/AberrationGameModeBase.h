9c0acdb71d0f2d70088019d2f6c6d1e94369b916
Neverhaste <161453529+Neverhaste@users.noreply.github.com>
2024-02-29 10:49:17 +0100

    UE Project and UE style .gitignore

---
+++ Aberration/Source/Aberration/AberrationGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AberrationGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ABERRATION_API AAberrationGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
